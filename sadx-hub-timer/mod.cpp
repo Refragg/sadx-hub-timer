5efea2da49c4277c91db95863716da4dd2eb057b
Refrag <R3FR4GYT@gmail.com>
2023-02-16 23:17:17 +0100

    Refactor button handling and make it configurable
    This patch is making the code cleaner and adding the ability to modify the bindings for the various actions related to the mod

--- sadx-hub-timer/mod.cpp
+++ sadx-hub-timer/mod.cpp
@@ -9,8 +9,41 @@
 #define true 1
 #define false 0
 
+#define B_A Buttons_A
+#define B_B Buttons_B
+#define B_X Buttons_X
+#define B_Y Buttons_Y
+#define B_S Buttons_Start
+#define B_S2 Buttons_D
+
+#define B_DD Buttons_Down
+#define B_DU Buttons_Up
+#define B_DL Buttons_Left
+#define B_DR Buttons_Right
+
+#define B_LB Buttons_C
+#define B_RB Buttons_Z
+
+#define B_LT Buttons_L
+#define B_RT Buttons_R
+
+#define OPTIONS_A "A"
+#define OPTIONS_B "B"
+#define OPTIONS_X "X"
+#define OPTIONS_Y "Y"
+#define OPTIONS_S "Start"
+#define OPTIONS_S2 "Back"
+#define OPTIONS_DU "DUp"
+#define OPTIONS_DD "DDown"
+#define OPTIONS_DL "DLeft"
+#define OPTIONS_DR "DRight"
+#define OPTIONS_LB "LB"
+#define OPTIONS_RB "RB"
+#define OPTIONS_LT "LT"
+#define OPTIONS_RT "RT"
+
 extern "C"
-{	
+{
 	//DataPointer(int, GlobalFrameCounter, 0x03B2C6C8);
 	static const char *TimerStateStrings[2] = 
 	{
@@ -19,6 +52,7 @@ extern "C"
 	
 	int previousGameState = 0;
 	int previousHeldButtons = 0;
+	int currentHeldButtons = 0;
 	int previousFrameCounter = 0;
 	
 	int elapsedFrames = 0;
@@ -31,6 +65,17 @@ extern "C"
 	bool timeThePauses = false;
 	
 	int color = 0;
+
+	Buttons resetTimerButton;
+	Buttons stopTimerButton;
+
+	Buttons hubsToTimePlusButton;
+	Buttons hubsToTimeMinusButton;
+
+	Buttons modifierButton;
+
+	Buttons pausesTimingButton;
+	Buttons manualStartButton;
 	
 	void DrawDebugRectangle(float leftchars, float topchars, float numchars_horz, float numchars_vert)
 	{
@@ -52,17 +97,62 @@ extern "C"
 		
 		SetDebugFontSize(FontScale*scale);
 	}
+
+	Buttons GetButton(std::string buttonString)
+	{
+#define OPTIONS_COMP(option, button) if (buttonString == option) return button;
+		OPTIONS_COMP(OPTIONS_A, B_A)
+		OPTIONS_COMP(OPTIONS_B, B_B)
+		OPTIONS_COMP(OPTIONS_X, B_X)
+		OPTIONS_COMP(OPTIONS_Y, B_Y)
+		OPTIONS_COMP(OPTIONS_S, B_S)
+		OPTIONS_COMP(OPTIONS_S2, B_S2)
+		OPTIONS_COMP(OPTIONS_A, B_A)
+		OPTIONS_COMP(OPTIONS_DU, B_DU)
+		OPTIONS_COMP(OPTIONS_DD, B_DD)
+		OPTIONS_COMP(OPTIONS_DL, B_DL)
+		OPTIONS_COMP(OPTIONS_DR, B_DR)
+		OPTIONS_COMP(OPTIONS_LB, B_LB)
+		OPTIONS_COMP(OPTIONS_RB, B_RB)
+		OPTIONS_COMP(OPTIONS_LT, B_LT)
+		OPTIONS_COMP(OPTIONS_RT, B_RT)
+#undef OPTIONS_COMP
+	}
 	
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer, nullptr, nullptr, 0, nullptr, 0, nullptr, 0, nullptr, 0 };
 	
 	__declspec(dllexport) void Init(const char* path, const HelperFunctions&)
 	{
+		const IniFile* configFile = new IniFile(std::string(path) + "\\config.ini");
+
+		stopTimerButton = GetButton(configFile->getString("Settings", "StopTimerButton", OPTIONS_LB));
+		resetTimerButton = GetButton(configFile->getString("Settings", "ResetTimerButton", OPTIONS_RB));
+
+		hubsToTimePlusButton = GetButton(configFile->getString("Settings", "HubsToTimePlusButton", OPTIONS_RB));
+		hubsToTimeMinusButton = GetButton(configFile->getString("Settings", "HubsToTimeMinusButton", OPTIONS_LB));
+
+		modifierButton = GetButton(configFile->getString("Settings", "ModifierButton", OPTIONS_Y));
+
+		pausesTimingButton = GetButton(configFile->getString("Settings", "PausesTimingButton", OPTIONS_LB));
+		manualStartButton = GetButton(configFile->getString("Settings", "ManualStartButton", OPTIONS_RB));
+
+		delete configFile;
+
 		previousGameState = GameState;
+	}
+
+	inline bool IsButtonPressed(Buttons button)
+	{
+		return (currentHeldButtons & button) != 0 && (previousHeldButtons & button) == 0;
+	}
+
+	inline bool IsButtonHeld(Buttons button)
+	{
+		return (currentHeldButtons & button) != 0;
 	}
 	
 	__declspec(dllexport) void __cdecl OnFrame()
 	{
-		
 		int deltaTime = FrameCounter - previousFrameCounter;
 
 		float time = timer / 60.0;
@@ -119,31 +209,37 @@ extern "C"
 			if (GameState != 16 && timeThePauses == false)
 				timer++;
 		}
+
+		currentHeldButtons = ControllerPointers[0]->HeldButtons;
 		
-		if (GameState != 16)
-		{			
-			if ((ControllerPointers[0]->HeldButtons & Buttons_C) != 0 && (previousHeldButtons & Buttons_C) == 0)
+		if (GameState != 16) // In-Game
+		{
+			if (IsButtonPressed(stopTimerButton))
 				isStarted = false;
-			if ((ControllerPointers[0]->HeldButtons & Buttons_Z) != 0 && (previousHeldButtons & Buttons_Z) == 0)
+
+			if (IsButtonPressed(resetTimerButton))
 				timer = 0;
 		}
-		else
+		else // Paused
 		{
-			if ((ControllerPointers[0]->HeldButtons & Buttons_C) != 0 && (previousHeldButtons & Buttons_C) == 0 && (ControllerPointers[0]->HeldButtons & Buttons_Y) == 0 && (ControllerPointers[0]->HeldButtons & Buttons_Start) == 0)
+			if (IsButtonPressed(stopTimerButton) && !IsButtonHeld(modifierButton) && !IsButtonHeld(B_S))
 				isStarted = false;
-			if ((ControllerPointers[0]->HeldButtons & Buttons_Z) != 0 && (previousHeldButtons & Buttons_Z) == 0 && (ControllerPointers[0]->HeldButtons & Buttons_Y) == 0 && (ControllerPointers[0]->HeldButtons & Buttons_Start) == 0)
+
+			if (IsButtonPressed(resetTimerButton) && !IsButtonHeld(modifierButton) && !IsButtonHeld(B_S))
 				timer = 0;
-			if ((ControllerPointers[0]->HeldButtons & Buttons_C) != 0 && (previousHeldButtons & Buttons_C) == 0 && (ControllerPointers[0]->HeldButtons & Buttons_Start) != 0)
+
+			if (IsButtonPressed(hubsToTimeMinusButton) && IsButtonHeld(B_S))
 			{
 				if (hubsToTime > 1)
 					hubsToTime--;
 			}
-			if ((ControllerPointers[0]->HeldButtons & Buttons_Z) != 0 && (previousHeldButtons & Buttons_Z) == 0 && (ControllerPointers[0]->HeldButtons & Buttons_Start) != 0)
+			if (IsButtonPressed(hubsToTimePlusButton) && IsButtonHeld(B_S))
 			{
 				if (hubsToTime < 9)
 					hubsToTime++;
 			}
-			if ((ControllerPointers[0]->HeldButtons & Buttons_C) != 0 && (previousHeldButtons & Buttons_C) == 0 && (ControllerPointers[0]->HeldButtons & Buttons_Y) != 0)
+
+			if (IsButtonPressed(pausesTimingButton) && IsButtonHeld(modifierButton))
 			{
 				if (timeThePauses == false)
 					timeThePauses = true;
@@ -151,10 +247,10 @@ extern "C"
 					timeThePauses = false;
 			}
 			
-			if ((((ControllerPointers[0]->HeldButtons & Buttons_Z) != 0) && (previousHeldButtons & Buttons_Z) == 0) && ((ControllerPointers[0]->HeldButtons & Buttons_Y) != 0))
+			if (IsButtonPressed(manualStartButton) && IsButtonHeld(modifierButton))
 				isStarted = true;
 		}
-		
+
 		previousGameState = GameState;
 		previousHeldButtons = ControllerPointers[0]->HeldButtons;
 		previousFrameCounter = FrameCounter;
